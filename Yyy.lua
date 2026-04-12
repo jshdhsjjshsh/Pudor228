c43507969429149a380f4e986031b2a5456b0fbf
jshdhsjjshsh <misterricik8@gmail.com>
2026-04-12 20:17:30 +0300

    Create Yyy.lua

---
+++ Yyy.lua
@@ -0,0 +1,12 @@
+-- ========== NEO CASE FARM BY ROBIN ==========
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local UserInputService = game:GetService("UserInputService")
+local RunService = game:GetService("RunService")
+local HttpService = game:GetService("HttpService")
+
+-- ВСЕ КЕЙСЫ
+local AllCases = {
+    "Trash", "Daily", "Photon Core", "Divine", "Beggar", "Plodder",
+    "Office Clerk", "Manager", "Director", "Oligarch", "
